6fad6182a238ee5b5d9eba42e5b9cb7599be93f6
Sophie Théréau <sophiethereau@hotmail.fr>
2025-02-11 19:44:48 +0100

    Add some rules to level up and chenge some tests

--- src/app/level-up/player.spec.ts
+++ src/app/level-up/player.spec.ts
@@ -2,6 +2,19 @@ import {Player} from "./player";
 
 describe('LevelUpComponent', () => {
 
+    it('should start with level 0 and experience 0 by default', () => {
+        let player = new Player();
+        expect(player.level).toBe(0);
+        expect(player.experience).toBe(0);
+    });
+
+    it('experience and level should be at 0 in case of undefined values', () => {
+        let player = new Player(undefined, undefined);
+
+        expect(player.level).toBe(0);
+        expect(player.experience).toBe(0);
+    });
+
     it('should gain 10 experience point each day', () =>{
         let player = new Player(0,0);
         player.newDay();
@@ -14,13 +27,13 @@ describe('LevelUpComponent', () => {
         expect(player.level).toBe(1);
     });
 
-    it('should keep the same level', () => {
+    it('should keep the same level if level is 10', () => {
         let player = new Player(10,10);
         player.newDay();
         expect(player.level).toBe(10);
     });
 
-    it('max level should be 10', () => {
+    it('should have a max level at 10', () => {
         let player = new Player(10,100);
         player.newDay();
         expect(player.level).toBeLessThanOrEqual(10);
@@ -32,38 +45,21 @@ describe('LevelUpComponent', () => {
         expect(player.level).toBeLessThan(11);
     });
 
-    it('should never have initial experience very high', () => {
-        let player = new Player(0,300);
+    it('should reset negative experience to 0 and win 10 points', () =>{
+        let player = new Player(0,-10);
         player.newDay();
-        expect(player.experience).not.toBeLessThanOrEqual(100);
+        expect(player.experience).toBe(10);
     });
 
-    it('should never have negative experience', () =>{
-        let player = new Player(10,-10);
-        player.newDay();
-        expect(player.experience).toBeGreaterThanOrEqual(0);
-    });
-
-    it('should never have negative level', () =>{
+    it('should reset negative level to 0', () =>{
         let player = new Player(-10,10);
         player.newDay();
-        expect(player.level).toBeGreaterThanOrEqual(0);
-    });
-
-    it('experience and level should be a valid number', () => {
-        const player = new Player(1, 50);
-
-        expect(typeof player.level).toBe('number');
-        expect(typeof player.experience).toBe('number');
-
-        player.newDay();
-        expect(typeof player.level).toBe('number');
-        expect(typeof player.experience).toBe('number');
+        expect(player.level).toBe(0);
     });
 
     describe('experience excess', () => {
         it('should keep the experience excess after level up', () => {
-            let player = new Player(0,103);
+            let player = new Player(0,203);
             player.newDay();
             expect(player.experience).toBe(13);
         });
--- src/app/level-up/player.ts
+++ src/app/level-up/player.ts
@@ -3,33 +3,37 @@ export class Player {
   level: number;
   experience: number;
 
-  constructor(level: number, experience: number) {
+  // Rules :
+  // Default level et experience are at 0.
+  // Each day, the player wins 10 points of experience.
+  // 100 points of experience corresponds to 1 level.
+  // Player keep its experience excess when he wins a level.
+  // Level stops growing when he's at 10.
+  // Player stops to win experience when he's at level 10.
+
+  constructor(level: number = 0, experience: number = 0) {
     this.level = level;
     this.experience = experience;
   }
 
 
   newDay(){
-    if(this.experience > 100){
-      this.resetExperience();
+    if (this.level >= 10) {
+      return;
     }
-    this.updateExperience();
 
     if(this.experience < 0){
-      this.resetExperience();
+      this.experience = 0;
     }
     if(this.level < 0){
       this.level = 0
     }
 
-    if(this.experience >= 100 && this.level < 10){
-      this.updateLevel();
-      this.resetExperience();
-    }
-  }
+    this.updateExperience();
 
-  resetExperience(){
-    this.experience -= 100;
+    while(this.experience >= 100 && this.level < 10){
+      this.updateLevel();
+    }
   }
 
   updateExperience(){
@@ -37,6 +41,7 @@ export class Player {
   }
 
   updateLevel(){
-    this.level += 1
+    this.level += 1;
+    this.experience = this.experience % 100;
   }
 }
