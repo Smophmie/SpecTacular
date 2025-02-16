0506546cfa5f144e388ff632b6e85894364a6c21
Sophie Théréau <sophiethereau@hotmail.fr>
2025-02-16 13:24:38 +0100

    first test spy expert ok

--- src/app/spy-expert/spy-expert.component.spec.ts
+++ src/app/spy-expert/spy-expert.component.spec.ts
@@ -1,14 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AdvicesService } from './core/services/advices/advices.service';
 import { provideHttpClient } from '@angular/common/http';
 import {
   HttpTestingController,
   provideHttpClientTesting,
 } from '@angular/common/http/testing';
+import {SpyExpertComponent} from "./spy-expert.component";
+import {Advice} from "./core/models/advice";
 
 describe('SpyExpertComponent', () => {
   let adviceService: AdvicesService;
   let httpTesting: HttpTestingController;
+  let fixture: ComponentFixture<SpyExpertComponent>;
+  let component: SpyExpertComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,14 +21,26 @@ describe('SpyExpertComponent', () => {
 
     httpTesting = TestBed.inject(HttpTestingController);
     adviceService = TestBed.inject(AdvicesService);
+    fixture = TestBed.createComponent(SpyExpertComponent);
+    component = fixture.componentInstance;
   });
 
   afterEach(() => {
     httpTesting.verify();
   });
   it('should return expected advice (httpclient called once)', () => {
+    const mockAdvice: Advice = { slip: { id: 1, advice: 'Test advice' } };
 
+    fixture.detectChanges();
+
+    const req = httpTesting.expectOne('https://api.adviceslip.com/advice');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockAdvice);
+
+    expect(component.advice).toEqual(mockAdvice);
   });
 
-  it('should properly handle request error', () => { });
+  it('should properly handle request error', () => {
+  });
 });
